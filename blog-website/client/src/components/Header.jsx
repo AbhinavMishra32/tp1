e8e1706dae8c77d959a3d964c4541a919aa8e434
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-07-23 23:18:22 +0530

    chore: Update npm dependencies and configuration files

--- blog-website/client/src/components/Header.jsx
+++ blog-website/client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { Navbar, TextInput, Button } from 'flowbite-react'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { FaMoon } from 'react-icons/fa'
 
 export default function Header() {
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
       <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
@@ -27,24 +28,24 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='purpleToBlue' outline>
             Sign In
           </Button>
         </Link>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link>
+        <Navbar.Link active={path === "/"} as={"div"}>
           <Link to='/'>
             Home
           </Link>
         </Navbar.Link>
-        <Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to='/about'>
             About
           </Link>
         </Navbar.Link>
-        <Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
           <Link to='/projects'>
             Projects
           </Link>
