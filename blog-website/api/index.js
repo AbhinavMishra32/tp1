e6defed999348eded0c1ec2f16485f8c933c824c
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-07-24 19:23:33 +0530

    chore: Update MongoDB connection URI and add PORT configuration

--- blog-website/api/index.js
+++ blog-website/api/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT || 6001;
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+mongoose.connect(process.env.MONGO_URI,).then(() => {
+  app.listen(PORT, () => console.log("Server Port: ", PORT));
+}).catch((error) => console.log(error));
