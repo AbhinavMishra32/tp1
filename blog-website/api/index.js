1f981b83a425299e5602c89983b7d47bfd807a29
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-07-24 21:16:28 +0530

    chore: Add user routes to API

--- blog-website/api/index.js
+++ blog-website/api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 const app = express();
 
@@ -10,3 +11,7 @@ const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URI,).then(() => {
   app.listen(PORT, () => console.log("Server Port: ", PORT));
 }).catch((error) => console.log(error));
+
+
+app.use('/api/user', userRoutes);
+
